0199ca5ba291294cb2ec6e6709f149638e542dbe
bhavinsavajiyani <bhavinsavajiyani9@gmail.com>
2019-11-23 02:32:37 +0530

    Implemented Feature: Infinite Ground Generation

---
+++ Assets/Scripts/Block Scripts/GroundBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundBlock : MonoBehaviour
+{
+    public Transform otherBlock;
+    public float halfLength = 100f;
+    private Transform _player;
+    private float endOffset = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveGround();
+    }
+
+    void MoveGround()
+    {
+        if(transform.position.z + halfLength < _player.transform.position.z - endOffset)
+        {
+            transform.position = new Vector3(otherBlock.position.x, otherBlock.position.y, 
+                otherBlock.position.z + halfLength * 2);
+        }
+    }
+}
